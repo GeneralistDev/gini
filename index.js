95cd2c42cdaf00d66289cc3d905c29ad07d08b62
Luke Horvat <lukehorvat@gmail.com>
2014-12-03 02:35:49 +1100

    Minor optimization by removing function to calculate mean

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 "use strict";
 
-function mean(array) {
-  var sum = 0;
-
-  array.forEach(function(value) {
-    sum += Number(value);
-  });
-
-  return sum / array.length;
-}
-
 module.exports.ordered = function(array) {
-  var sum = 0;
+  var sum1 = 0;
+  var sum2 = 0;
+
   array.forEach(function(value, i) {
-    sum += ((2 * (i + 1)) - array.length - 1) * Number(value);
+    sum1 += ((2 * (i + 1)) - array.length - 1) * Number(value);
+    sum2 += Number(value);
   });
 
-  return sum / (Math.pow(array.length, 2) * mean(array));
+  return sum1 / (Math.pow(array.length, 2) * (sum2 / array.length));
 };
 
 module.exports.unordered = function(array) {
-  var sum = 0;
+  var sum1 = 0;
+  var sum2 = 0;
 
   array.forEach(function(value1, i) {
     array.forEach(function(value2, j) {
       if (i != j) {
-        sum += Math.abs(Number(value1) - Number(value2));
+        sum1 += Math.abs(Number(value1) - Number(value2));
       }
     });
+
+    sum2 += Number(value1);
   });
 
-  return sum / (2 * Math.pow(array.length, 2) * mean(array));
+  return sum1 / (2 * Math.pow(array.length, 2) * (sum2 / array.length));
 };
